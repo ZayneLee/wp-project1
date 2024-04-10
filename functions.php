de3092973786669f776abcd36be9f451942a8bdc
Zayne <star1582@naver.com>
2024-04-10 13:42:35 +0100

    Enqueue style

--- functions.php
+++ functions.php
@@ -5,3 +5,9 @@ function myfirstwp_setup()
 {
     add_theme_support('post-thumbnails');
 }
+
+add_action('wp_enqueue_scripts', 'myfirstwp_first');
+function myfirstwp_first()
+{
+    wp_enqueue_style('theme-style', get_stylesheet_uri());
+}
--- index.php
+++ index.php
@@ -10,22 +10,43 @@
 </head>
 
 <body>
-    <?php
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post(); ?>
+    <div class="container"></div>
+    <div class="section-content">
+        <?php
+        if (have_posts()) { ?>
+            <main>
+                <?php while (have_posts()) {
+                    the_post(); ?>
 
-            <a href="<?php the_permalink() ?>">
-                <?php the_title(); ?>
-            </a>
-            <img src="<?php echo esc_url(wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium')[0]); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true) ?>" class="post-featured-image">
-    <?php
-            the_excerpt();
+                    <article id="post">
+                        <div class="post-header">
+                            <h4>
+                                <a href="<?php the_permalink() ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h4>
+                            <div class="post-meta">
+                                <!-- TODO: post meta here -->
+                            </div>
+                        </div>
+                        <div class="post-body">
+                            <a href="<?php the_permalink(); ?>" class="post-featured">
+                                <img src="<?php echo esc_url(wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium')[0]); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true) ?>" class="post-featured-image">
+                            </a>
+                            <div class="post-text-with-featured">
+                                <?php the_excerpt(); ?>
+                            </div>
+                        </div>
+                    </article>
+
+                <?php } ?>
+            </main>
+        <?php } else {
+            echo 'No posts';
         }
-    } else {
-        echo 'No posts';
-    }
-    ?>
+        ?>
+    </div>
+
 
     <?php wp_footer() ?>
 
