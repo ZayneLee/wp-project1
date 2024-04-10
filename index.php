464ebc644c8e08ec2ad07024538d1176cdb85852
Zayne <star1582@naver.com>
2024-04-10 14:11:45 +0100

    Index page style

--- index.php
+++ index.php
@@ -9,42 +9,43 @@
 
 </head>
 
-<body>
-    <div class="container"></div>
-    <div class="section-content">
-        <?php
-        if (have_posts()) { ?>
-            <main>
-                <?php while (have_posts()) {
-                    the_post(); ?>
+<body <?php body_class() ?>>
+    <div class="container">
+        <div class="section-content">
+            <?php
+            if (have_posts()) { ?>
+                <main>
+                    <?php while (have_posts()) {
+                        the_post(); ?>
 
-                    <article id="post">
-                        <div class="post-header">
-                            <h4>
-                                <a href="<?php the_permalink() ?>">
-                                    <?php the_title(); ?>
+                        <article id="post">
+                            <div class="post-header">
+                                <h4>
+                                    <a href="<?php the_permalink() ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </h4>
+                                <div class="post-meta">
+                                    <!-- TODO: post meta here -->
+                                </div>
+                            </div>
+                            <div class="post-body">
+                                <a href="<?php the_permalink(); ?>" class="post-featured">
+                                    <img src="<?php echo esc_url(wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium')[0]); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true) ?>" class="post-featured-image">
                                 </a>
-                            </h4>
-                            <div class="post-meta">
-                                <!-- TODO: post meta here -->
+                                <div class="post-text-with-featured">
+                                    <?php the_excerpt(); ?>
+                                </div>
                             </div>
-                        </div>
-                        <div class="post-body">
-                            <a href="<?php the_permalink(); ?>" class="post-featured">
-                                <img src="<?php echo esc_url(wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium')[0]); ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id($post->ID), '_wp_attachment_image_alt', true) ?>" class="post-featured-image">
-                            </a>
-                            <div class="post-text-with-featured">
-                                <?php the_excerpt(); ?>
-                            </div>
-                        </div>
-                    </article>
+                        </article>
 
-                <?php } ?>
-            </main>
-        <?php } else {
-            echo 'No posts';
-        }
-        ?>
+                    <?php } ?>
+                </main>
+            <?php } else {
+                echo 'No posts';
+            }
+            ?>
+        </div>
     </div>
 
 
